05a7dd86a8d7dbd46ab2186f4e50bb2dbf29fb45
jarek <loop123jk@gmail.com>
2026-03-17 14:23:27 -0600

    grid with buttons added

--- src/meta_components/grid_with_buttons/grid_with_buttons.test.tsx
+++ src/meta_components/grid_with_buttons/grid_with_buttons.test.tsx
@@ -3,6 +3,7 @@ import { test } from 'vitest'
 import {Api} from "../../utility/api";
 import {GridWithButtons} from "./grid_with_buttons";
 import {Button} from "@mui/material";
+import {ApiButton} from "../../components/button/button";
 
 test('should load and display server-side data', async () => {
 
@@ -10,7 +11,8 @@ test('should load and display server-side data', async () => {
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
-    render(<GridWithButtons api={mockApi} grid_endpoint={"list"} endpoints={["trigger"]} buttons={[
+    render(<GridWithButtons api={mockApi} grid_endpoint={"list"} buttons={[
+        <ApiButton api={mockApi} endpoint={"trigger"} />,
         <Button>Test1</Button>,
         <Button>Test2</Button>,
     ]}/>)
--- src/meta_components/grid_with_buttons/grid_with_buttons.tsx
+++ src/meta_components/grid_with_buttons/grid_with_buttons.tsx
@@ -2,26 +2,22 @@ import type {FC, ReactElement, RefObject} from "react";
 import {type TableState, UITable} from "../../components/grid/grid";
 import type {ApiClient} from "../../utility/api";
 import {Stack} from "@mui/material";
-import {ApiButton} from "../../components/button/button";
 
 export interface GridWithButtonsProps {
     api: ApiClient;
-    ref: RefObject<TableState>;
-
+    grid_ref: RefObject<TableState>;
     grid_endpoint: string;
-    endpoints: string[];
-    buttons: ReactElement[] | undefined;
+
+    buttons: ReactElement[];
 }
 
-
-export const GridWithButtons: FC<GridWithButtonsProps> = ({api, ref, grid_endpoint, endpoints, buttons}) => {
+export const GridWithButtons: FC<GridWithButtonsProps> = ({api, grid_ref, grid_endpoint, buttons}) => {
     return (
         <Stack direction={"column"} gap={3}>
             <Stack direction={"row"} gap={3}>
                 {buttons?.map((button) => button)}
-                {endpoints.map((endpoint) => <ApiButton api={api} endpoint={endpoint} />)}
             </Stack>
-            <UITable ref={ref} api={api} endpoint={grid_endpoint} />
+            <UITable ref={grid_ref} api={api} endpoint={grid_endpoint} />
         </Stack>
     )
 }
