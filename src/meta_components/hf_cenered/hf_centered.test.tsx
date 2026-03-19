79a9ab1831bf607a86a73858ad6bb8b6c260ec5a
jarek <loop123jk@gmail.com>
2026-03-18 18:16:15 -0600

    styling

--- src/meta_components/hf_cenered/hf_centered.test.tsx
+++ src/meta_components/hf_cenered/hf_centered.test.tsx
@@ -3,7 +3,7 @@ import { test } from 'vitest'
 import {HFCenteredLayout} from "./hf_centered";
 import {GridWithButtons} from "../grid_with_buttons/grid_with_buttons";
 import {Api} from "../../utility/api";
-import {Button, Paper} from "@mui/material";
+import {Button, Paper, Typography} from "@mui/material";
 import {UIInput} from "../../components/upload_input/upload_input";
 import {CenteredContainer} from "../../components/centered_container/centered_container";
 
@@ -11,6 +11,15 @@ test('should load and display server-side data', async () => {
     const mockApi = Api({endpoint: "http://localhost:8000"})
 
     render(<HFCenteredLayout>
+
+        <CenteredContainer  sx={{ minHeight: "10vh", display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography   align="center" sx={{ fontWeight: 'bold', width: '100%', px: 2}} >
+                HTS Code Lookup
+            </Typography>
+            <Typography  align="center" sx={{ width: '100%', px: 2 }}>
+                Upload CSV or paste / type in part numbers (one per line or comma-separated).
+            </Typography>
+        </CenteredContainer>
         <CenteredContainer  sx={{minHeight: "10hv"}}>
             <UIInput />
         </CenteredContainer>
